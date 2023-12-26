73c26292e6825e33e12dfec8101106e44cca6097
Pedro Germano <pedrogermano232@gmail.com>
2023-12-25 22:27:15 -0300

    date

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,20 +1,25 @@
+import { format } from 'date-fns'
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ author, publishedAt }) {
+  const publishedDateFormatted = format()
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar  src="https://lh3.googleusercontent.com/a/ACg8ocI9lDZM1i4en6OGrvN9qVDeyVRVihio9KixEIyjsXVX9jPY=s288-c-no" />
+          <Avatar  src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Pedro Germano</strong>
-            <span>Front-end engineer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
+          {publishedDateFormatted}
+        </time>
       </header>
       <div className={styles.content}>
         <p>Fala galeraa 👋 </p>
